70ec7b0be504ece430e3e4c7f4204a551654810e
Dan Hedgecock <hedgecock.d@gmail.com>
2017-07-26 07:45:18 -0700

    Update to latest linting, use Prettier, handle .jsx files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,14 @@
 
 module.exports = {
   root: true,
-  extends: '@crystal-ball/eloquence'
-}
+  extends: '@crystal-ball/eloquence',
+
+  // Override for strict Node scripts
+  parserOptions: {
+    sourceType: 'script'
+  },
+
+  rules: {
+    strict: [1, 'global']
+  }
+};
