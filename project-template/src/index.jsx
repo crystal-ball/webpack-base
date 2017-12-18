19719109c923143763b3b657a0b27b96b9e3d835
Dan Hedgecock <hedgecock.d@gmail.com>
2017-12-18 15:27:37 -0800

    feat(icons): include local storage loader in demo project

--- project-template/src/index.jsx
+++ project-template/src/index.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 /* eslint-disable import/no-extraneous-dependencies */
+import localStorageSVGLoader from 'svg-symbol-sprite-loader/src/local-storage-svg-loader'
 import { AppContainer } from 'react-hot-loader'
 
 // Application core elements: View, State and Styles
 import App from './components/App'
 import createStore from './state-container/createStore'
 import './styles/index.scss'
+
+localStorageSVGLoader(window.manifest['icon-sprite.svg'])
 
 const render = Component => {
   ReactDOM.render(
