2ae07c95310b0d2d937b5c17d46ee1e2e35669af
Dan Hedgecock <hedgecock.d@gmail.com>
2017-12-18 15:27:00 -0800

    chore: enhance process for creating a test project
    Better test project generation, only npm install and npm start required to validate module

--- scripts/generate-test-project.js
+++ scripts/generate-test-project.js
@@ -7,11 +7,13 @@ const templatePackage = require('../project-template/package.json')
 const repoPackage = require('../package.json')
 
 const testProjectDir = resolve(__dirname, '../test-project')
+const webpackSrcDir = resolve(__dirname, '../src')
 const templateProjectDir = resolve(__dirname, '../project-template')
 const webpackConfigPath = join(templateProjectDir, 'webpack.config.js')
 
 // Copy template project files
 shell.cp('-R', templateProjectDir, testProjectDir)
+shell.cp('-R', webpackSrcDir, join(testProjectDir, 'lib'))
 
 // Add the dependencies from the project to the dev dependencies of the test project
 // Normally these dependencies will be pulled down as part of this module
@@ -27,12 +29,19 @@ writeFile(
   () => {},
 )
 
+writeFile(
+  join(testProjectDir, 'index.js'),
+  `require = require('@std/esm')(module)
+module.exports = require('./lib').default`,
+  () => {},
+)
+
 // Update the webpack config to use configs from this repo
 readFile(webpackConfigPath, (err, data) => {
   if (err) return console.warn(err)
 
   let file = data.toString()
-  file = file.replace('@inspirescript/webpack-configs', '../index')
+  file = file.replace('@inspirescript/webpack-configs', './index')
 
   return writeFile(join(testProjectDir, 'webpack.config.js'), file, () => {})
 })
