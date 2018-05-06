2342a73bd1fe80c3ef591f04947a86db0e24f7c8
Dan Hedgecock <hedgecock.d@gmail.com>
2018-05-06 16:28:20 -0700

    chore: disable import rules in test-app

--- test-app/.eslintrc.js
+++ test-app/.eslintrc.js
@@ -5,9 +5,11 @@ module.exports = {
   root: true,
   extends: 'eloquence',
   rules: {
-    'import/no-unresolved': 'off',
     'import/extensions': 'off',
-    'import/no-unresolved': 'off',
+    'import/no-duplicates': 'off',
     'import/no-extraneous-dependencies': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-unresolved': 'off',
   },
 }
