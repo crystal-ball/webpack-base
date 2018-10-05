3c7bb134fdec1d310c28dc328018b88085e093c8
Dan Hedgecock <dan.hedgecock@nodesource.com>
2018-10-05 05:17:26 -0700

    feat: babel upgrade to v7 🎉
    BREAKING CHANGE:
    Babel config files and additional propasals must be updated to v7 syntax+packages

--- test-app/src/lib/logger.js
+++ test-app/src/lib/logger.js
@@ -2,7 +2,7 @@
 /**
  * Example application wide utility method.
  */
-export default (message: string) => {
+export default message => {
   if (process.env.DEBUG) console.log('Extra logging: ')
   console.info(message)
 }
