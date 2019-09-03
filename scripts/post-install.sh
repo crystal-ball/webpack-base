d64cd77b54c98903864127cfbfec8d7bd734c396
Dan Hedgecock <Hedgecock.D@gmail.com>
2019-09-03 09:47:58 -0700

    Fix: Postinstall script fix attempt 3 😅 (#71)

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # This post install script will install Cypress after local installs but not
-# in CI/CD environments where Cypress is run using a Docker image. This helps
-# speed up CI/CD builds by only installing Cypress where necessary.
+# in CI/CD environments where Cypress will be run using a Docker image (This
+# helps speed up CI/CD builds by only installing Cypress where necessary).
+
+# Skip install in continous integration envs
+[[ ! -z "$CI" ]] && exit 0
 
 # When package is installed as a node_module, do nothing
 [[ "$PWD" == *node_modules* ]] && exit 0
 
-# Check for Travis env variable to determine CI/CD state
-if [ -z "$TRAVIS_CI" ]; then
-  echo Installing Cypress...
-  npm install cypress@3.4.1 --no-save
-fi
+# NPM install was run in local project repo, install Cypress for local testing √
+echo Installing Cypress...
+npm install cypress@3.4.1 --no-save
