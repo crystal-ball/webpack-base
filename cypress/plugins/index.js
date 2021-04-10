3c19741ace1a53fa92ddde2adb44746fd003842e
Dan Hedgecock <Hedgecock.D@gmail.com>
2021-04-10 08:20:31 -0700

    Upgrade: Percy version updates (#284)

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,12 +10,11 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
 const percyHealthCheck = require('@percy/cypress/task')
 
-module.exports = (on /*  config */) => {
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+module.exports = (on /* config */) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
