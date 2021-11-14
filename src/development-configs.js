c1087413bcbb31f7f140ae9e3b57f62db26cdb5f
Dan Hedgecock <hedgecock.d@gmail.com>
2021-11-14 09:51:33 -0800

    Update packages

--- src/development-configs.js
+++ src/development-configs.js
@@ -6,9 +6,9 @@ module.exports = ({ devServer, devtool, paths }) => ({
   // to see generated code,)
   devtool: devtool || 'eval-source-map',
 
-  // Development plugins
-  // ---------------------------------------------------------------------------
-  plugins: ['hotModuleReplacementPlugin', 'friendlyErrorsPlugin'],
+  stats: {
+    logging: 'warn',
+  },
 
   // webpack-dev-server
   // ---------------------------------------------------------------------------
@@ -18,29 +18,24 @@ module.exports = ({ devServer, devtool, paths }) => ({
    * File serving debug info served at /webpack-dev-server
    */
   devServer: {
-    // ℹ️ clientLogLevel - Suppress logging, the FriendlyErrors plugin displays
-    // cleaner messaging Controls the console logs in the browser before
-    // reloading, HMR, etc.
-
-    // Serve static file from the public file (only required for files not
-    // imported into project)
-    contentBase: paths.static,
+    client: {
+      // Show compilation errors and warnings
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
+    },
     // Serve index.html for all unmatched routes
     historyApiFallback: true,
     // Enable hot module replacement feature
     hot: true,
-    // Supress messages output, they're managed by the ProgressBarPlugin and the
-    // FriendlyErrors plugin
-    noInfo: true,
     // Disable auto-open until a re-use tab solution is figured out
     open: false,
-    // Show compilation errors and warnings
-    overlay: {
-      warnings: false,
-      errors: true,
-    },
     // The.port.
     port: 3000,
+    // Serve static file from the public file (only required for files not
+    // imported into project)
+    static: paths.static,
     // Custom overrides
     ...devServer,
   },
